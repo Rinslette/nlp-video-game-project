692e5fc4f4ba8d17850f12ca492a901050f49e14
Rinslette <78517804+Rinslette@users.noreply.github.com>
2024-01-08 13:13:22 +0800

    projectNLP.py

--- CodingProjectNLP.py
+++ CodingProjectNLP.py
@@ -16,7 +16,7 @@ import re
 from sklearn.preprocessing import MultiLabelBinarizer
 
 # Load the data
-df = pd.read_csv('/vgsales_Clean.csv')
+df = pd.read_csv('./vgsales_Clean.csv')
 # Reduce dataframe to feature 'Name', and label 'Genre'
 df = df[['Name','Genre']].copy()
 df.head()
