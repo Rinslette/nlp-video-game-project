ae2e29baf638d883f2259537b3bee377c79b6411
Rinslette <78517804+Rinslette@users.noreply.github.com>
2024-01-13 16:56:06 +0800

    Update CodingProjectNLP.py

--- CodingProjectNLP.py
+++ CodingProjectNLP.py
@@ -20,8 +20,7 @@ page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://r4.wallpaperflare.com/wallpaper/96/92/869/game-games-2014-best-wallpaper-a94028fd717a4d2bd6c7181f7021068d.jpg");
-background-size: 180%;
-background-position: top left;
+background-size: cover;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
